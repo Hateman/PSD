9468bb21165f83dfe39f954d01579eaa9c7f5932
Alex <37542088+Hateman@users.noreply.github.com>
2019-06-26 23:31:38 +0300

    Add files via upload

---
+++ src/vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <!-- <app-header></app-header> -->
+  <app-content></app-content>
+</div>
+</template>
+
+<script>
+import Content from './Content.vue';
+// import Header from './Header.vue';
+
+
+export default {
+  components: {
+    'app-content': Content,
+    // 'app-header': Header,
+  },
+}
+
+</script>
+
+<style></style>
---
+++ src/vue/Content.vue
@@ -0,0 +1,100 @@
+<template>
+  <div >
+
+    <header>
+      <div class="head-container">
+        <table>
+          <tr>
+
+        <td class="td1">
+          <div class="social">
+          <img  src="../img/header/Vector Smart Object 1.png"/>
+          <img  src="../img/header/Vector Smart Object 2.png"/>
+          <img  src="../img/header/Vector Smart Object 3.png"/>
+          <img  src="../img/header/Vector Smart Object 4.png"/>
+          <img  src="../img/header/Vector Smart Object 5.png"/>
+        </div>
+        </td>
+
+        <td class="td2">
+          <p class="logo-top">
+          <img  src="../img/header/Logo-AgencyFB.png"/>
+        </p>
+        </td>
+
+        <td class="td3">
+          <p class="logo-top">
+          <img  src="../img/header/Menu.png"/>
+        </p>
+        </td>
+
+        </tr>
+        </table>
+      </div>
+    </header>
+
+    <div class="container-1">
+        <div class="tir-1">
+          <img  src="../img/tir-1/tir-1-1.jpg"/>
+          <img  src="../img/tir-1/tir-1-2.jpg"/>
+          <img  src="../img/tir-1/tir-1-3.jpg"/>
+          <img  src="../img/tir-1/tir-1-4.jpg"/>
+        </div>
+
+        <div class="tir-2">
+          <img  src="../img/tir-2/tir-2-1.jpg"/>
+          <img  src="../img/tir-2/tir-2-2.jpg"/>
+          <img  src="../img/tir-2/tir-2-3.jpg"/>
+          <img  src="../img/tir-2/tir-2-4.jpg"/>
+        </div>
+    </div>
+
+    <div class="container-2">
+      <div class="container-2-1">
+            <h1 style="font-family:YanoneKaffeesatz-Regular"> A Picture Is Worth A Thousand Words </h1>
+            <h3 style="font-family:Arial-Regular">People peter griffin high school mom bra just avenger le me students friendzoned challenge accepted. True Story face troll finals derpina cereal guy lose back final week yao male. Not bad luke feel like a sir bacon pizza coffee a the avengers puking rainbows nyan cat search.
+            </h3>
+            <img  src="../img/LearnMoreAboutUs.jpg"/>
+      </div>      
+    </div>
+
+    <div class="container-3">
+      <div class="container-3-1">
+            <img  src="../img/cont-3-1.jpg"/>
+      </div>  
+
+      <div class="container-3-2">
+            <!-- <img  src="../img/cont-3-1.jpg"/> -->
+      </div>     
+    </div>
+
+    <div class="container-4">
+      <div class="container-4-1">
+            <img  src="../img/cont-4-1.jpg"/>
+      </div>  
+
+      <div class="container-4-2">
+            <!-- <img  src="../img/cont-4-1.jpg"/> -->
+      </div>     
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+   data () {
+    return {
+    }
+  },
+  created() {
+  },
+  methods: {
+  },
+  computed: {
+  },
+}
+</script>
+
+<style src="./cont.css"></style>
