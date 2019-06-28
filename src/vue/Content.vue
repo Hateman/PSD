ebebcb4db881a9d9a4c6b3a78fb8020a620e4f35
Alex <37542088+Hateman@users.noreply.github.com>
2019-06-29 01:24:37 +0300

    Add files via upload

--- src/vue/Content.vue
+++ src/vue/Content.vue
@@ -32,7 +32,7 @@
         </table>
       </div>
     </header>
-
+<!-- container 1 - Gallery ============================================================================================= -->
     <div class="container-1">
         <div class="tir-1">
           <img  src="../img/tir-1/tir-1-1.jpg"/>
@@ -48,7 +48,7 @@
           <img  src="../img/tir-2/tir-2-4.jpg"/>
         </div>
     </div>
-
+<!-- conatiner 2 - About ======================================================================================================== -->
     <div class="container-2">
       <div class="container-2-1">
             <h1 style="font-family:YanoneKaffeesatz-Regular"> A Picture Is Worth A Thousand Words </h1>
@@ -57,26 +57,231 @@
             <img  src="../img/LearnMoreAboutUs.jpg"/>
       </div>      
     </div>
-
+<!-- container 3 - post #1 ======================================================================================================== -->
     <div class="container-3">
       <div class="container-3-1">
-            <img  src="../img/cont-3-1.jpg"/>
+        <table class="table-3"> <tr style="tr3">
+        <td class="td4"> <h7> A Picture Is Worth A Thousand Words </h7> </td>
+        <td class="td5"> <h7> 01/17/2014 </h7> </td>
+        <td class="td4"> <h7> 8 minutes read </h7> </td>
+        </tr> </table>
+
+            <table class="table-3"> <tr style="tr3">
+            <td class="td6"> <h7> A Picture Is Worth A Thousand Words </h7> </td>
+            <td class="td6"> <h7>  </h7> </td>
+            </tr> </table>
+              
+              <table class="table-4">
+                <tr>
+
+                  <td>
+                    <img  src="../img/cont-3-1.jpg"/>
+                  </td>
+
+                  <td>
+                    <h5 class="td7"> 
+                      Dad blizzard coffee unsave facepalm face feel like a sir tank mother aww yeah. Anonymous easter puking rainbows bear wodka simpson amnesia read cookie monster final week grey. 
+                    </h5>
+                    <h5 class="td8"> 
+                      Mom hammer creepy me gusta morbi eat le friend steve jobs cellphone clinton cereal guy. Good guy high school on okay a bacon homework in joke dog. Not Okay if you know what i mean nap monocle oh stop it, you pizza diablo 3 captain on weekend elephant. I Dont Get It drink ba dumm tss amet basic math female phone gentlemen strangers.
+                    </h5>  
+                  </td>
+
+                  <td class="td9">
+                    <h9>Author</h9><br>
+                    <h9 style="color: #ec1818; font-size: 10pt;">Jonas Duri</h9>
+                    <img  src="../img/vertical soc/face.png"/><br><br>
+                    <img  src="../img/vertical soc/1.png"/><br>
+                    <img  src="../img/vertical soc/2.png"/><br>
+                    <img  src="../img/vertical soc/3.png"/><br>
+                    <img  src="../img/vertical soc/4.png"/><br>
+                  </td>
+                </tr>
+              </table>
       </div>  
 
-      <div class="container-3-2">
-            <!-- <img  src="../img/cont-3-1.jpg"/> -->
-      </div>     
+        <div class="container-3-2">
+          <h4 style="font-family:YanoneKaffeesatz-Regular; text-align:center;">Categorys</h4><br>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 1.png"/>&nbsp&nbsp&nbsp Traveling (<span style="color:#dd1b1b">9</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt; color:#dd1b1b; margin-left:10px;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp A Picture is Worth A (..)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt; color:#dd1b1b; margin-left:10px"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Trip to New York City was (..)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Photos &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">28</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Lifestyle &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">4</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Inspiration &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">4</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Fashion &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">28</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Business &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">9</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Technology &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">16</span>)</h4>
+        </div>     
     </div>
+<!-- container 4 - Post #2 ======================================================================================================== -->
+<div class="container-3">
+      <div class="container-3-1">
+        <table class="table-3"> <tr style="tr3">
+        <td class="td4"> <h7> My First Trip to NYC </h7> </td>
+        <td class="td5"> <h7> 01/15/2014 </h7> </td>
+        <td class="td4"> <h7> 14 minutes read </h7> </td>
+        </tr> </table>
 
-    <div class="container-4">
-      <div class="container-4-1">
-            <img  src="../img/cont-4-1.jpg"/>
+            <table class="table-3"> <tr style="tr3">
+            <td class="td6"> <h7> Trip To New York Was Awesome </h7> </td>
+            <td class="td6"> <h7>  </h7> </td>
+            </tr> </table>
+              
+              <table class="table-4">
+                <tr>
+
+                  <td>
+                    <img  src="../img/cont-4-1.jpg"/>
+                  </td>
+
+                  <td>
+                    <h5 class="td7"> 
+                      Dad blizzard coffee unsave facepalm face feel like a sir tank mother aww yeah. Anonymous easter puking rainbows bear wodka simpson amnesia read cookie monster final week grey. 
+                    </h5>
+                    <h5 class="td8"> 
+                      Mom hammer creepy me gusta morbi eat le friend steve jobs cellphone clinton cereal guy. Good guy high school on okay a bacon homework in joke dog. Not Okay if you know what i mean nap monocle oh stop it, you pizza diablo 3 captain on weekend elephant. I Dont Get It drink ba dumm tss amet basic math female phone gentlemen strangers.
+                    </h5>  
+                  </td>
+
+                  <td class="td9">
+                    <h9>Author</h9><br>
+                    <h9 style="color: #ec1818; font-size: 10pt;">Jonas Duri</h9>
+                    <img  src="../img/vertical soc/face.png"/><br><br>
+                    <img  src="../img/vertical soc/1.png"/><br>
+                    <img  src="../img/vertical soc/2.png"/><br>
+                    <img  src="../img/vertical soc/3.png"/><br>
+                    <img  src="../img/vertical soc/4.png"/><br>
+                  </td>
+                </tr>
+              </table>
       </div>  
 
-      <div class="container-4-2">
-            <!-- <img  src="../img/cont-4-1.jpg"/> -->
-      </div>     
+        <div class="container-3-2">
+          <h4 style="font-family:YanoneKaffeesatz-Regular; text-align:center;">Archive</h4><br>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 1.png"/>&nbsp&nbsp&nbsp 2014 (<span style="color:#dd1b1b">4</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt; margin-left:5px"><img  src="../img/Polygon 1.png"/>&nbsp&nbsp&nbsp January (<span style="color:#dd1b1b">4</span>)</h4>
+
+          <h4 style="font-family:Arial; font-size: 12pt; color:#dd1b1b; margin-left:10px;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp A Picture is Worth A (..)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt; color:#dd1b1b; margin-left:10px"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp Trip to New York City was (..)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp February &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">28</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp March &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">4</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp April &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">4</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp May &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">28</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp June &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">9</span>)</h4>
+          <h4 style="font-family:Arial; font-size: 12pt;"><img  src="../img/Polygon 2.png"/>&nbsp&nbsp&nbsp July &nbsp&nbsp&nbsp(<span style="color:#dd1b1b">16</span>)</h4>
+        </div>     
     </div>
+
+    <footer>
+      <div class="foot-container">
+        <table>
+          <tr>
+
+        <td class="td1">
+          <div class="social">
+          <img  src="../img/footer/1.png"/>
+          <img  src="../img/footer/2.png"/>
+          <img  src="../img/footer/3.png"/>
+          <img  src="../img/footer/4.png"/>
+          <img  src="../img/footer/5.png"/>
+        </div>
+        </td>
+
+        <td class="td2">
+          <p class="logo-top">
+          <img  src="../img/footer/Logo-AgencyFB.png"/>
+        </p>
+        </td>
+
+        <td class="td3">
+          <p class="logo-top">
+          <img  src="../img/footer/menu.png"/>
+        </p>
+        </td>
+
+        </tr>
+        </table>
+      </div>
+    </footer>
+<!-- container 5 - Contact us + Random posts ======================================================================================================== -->
+    
+
+    <div class="container-5">
+      <div class="container-5-1">
+            <table>
+              <tr class="td10"> Contact Me </tr>
+              <tr class="td11"> Mom hammer creepy me gusta morbi eat le friend steve jobs </tr>
+              <tr class="td11"> cellphone clinton cereal guy. </tr><br>
+              
+              <tr><td class="td12">  <img  src="../img/name.jpg"/> 
+              <span class="td12" style="margin-left:20px; color:#6e7074">*required</span> </td>
+              </tr><br>
+
+              <tr><td class="td12">  <img  src="../img/email.jpg"/> </td></tr><br>
+              
+              <tr><td class="td12">  <img  src="../img/Message.jpg"/> </td></tr><br>
+
+              <tr><td style="text-align:left">  <img  src="../img/Send.jpg"/> </td></tr>
+
+            </table>
+      </div>  
+
+      <div class="container-5-2">
+              <div class="container-5-2-1">
+                <h4 class="td13">Random Post</h4>
+                <img  src="../img/5-1.jpg"/>
+                <h4 class="td14">Some advise on Photography</h4>
+              </div>
+
+                    <div class="container-5-2-2">
+                      <h4 class="td13">Random Post</h4>
+                      <img  src="../img/5-2.jpg"/>
+                      <h4 class="td14">Some advise on Photography</h4>
+                    </div>
+
+              <div class="container-5-2-3">
+                <img  style="margin-top:30px;" src="../img/header/Logo-AgencyFB.png"/><br><br>
+                <h4 class="td15">Newsletter</h4><br>
+                <img  src="../img/email.jpg"/><br><br>
+                <img  src="../img/Subscribe.jpg"/>
+              </div> 
+      </div>
+    </div>
+    
+    
+<!-- Footer ================================================================================================================ -->
+
+    <footer>
+      <div class="foot-container">
+        <table>
+          <tr>
+
+        <td class="td1">
+          <div class="social">
+          <img  src="../img/footer/1.png"/>
+          <img  src="../img/footer/2.png"/>
+          <img  src="../img/footer/3.png"/>
+          <img  src="../img/footer/4.png"/>
+          <img  src="../img/footer/5.png"/>
+        </div>
+        </td>
+
+        <td class="td2">
+          <p class="logo-top">
+          <img  src="../img/footer/Logo-AgencyFB.png"/>
+        </p>
+        </td>
+
+        <td class="td3">
+          <p class="logo-top">
+          <img  src="../img/footer/menu.png"/>
+        </p>
+        </td>
+
+        </tr>
+        </table>
+      </div>
+    </footer>
 
 
   </div>
